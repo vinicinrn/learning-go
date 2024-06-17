910201e72914af8d876688f8bb39ff3d7251b118
vinicinrn <zviniciusnascimento@gmail.com>
2024-06-17 18:56:35 -0300

    learning slices and arrays

--- soma/soma.go
+++ soma/soma.go
@@ -1,8 +1,8 @@
 package main
 
-func Soma(numeros [5]int) int {
+func Soma(numeros []int) int {
 	soma := 0
-	for _, numeros := range numeros {
+	for _, numeros := range numeros { //sempre que é chamado, retorna dois valores: indíce e valor
 		soma += numeros
 	}
 	return soma
--- soma/soma_test.go
+++ soma/soma_test.go
@@ -4,12 +4,26 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	numeros := [5]int{1, 2, 3, 4, 5}
+	t.Run("coleção de 5 números", func(t *testing.T) {
+		numeros := []int{1, 2, 3, 4, 5}
 
-	resultado := Soma(numeros)
-	esperado := 15
+		resultado := Soma(numeros)
+		esperado := 15
 
-	if esperado != resultado {
-		t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
-	}
+		if resultado != esperado {
+			t.Errorf("resultado %d, want %d, dado %v", resultado, esperado, numeros)
+		}
+	})
+
+	t.Run("coleção de qualquer tamanho", func(t *testing.T) {
+		numeros := []int{1, 2, 3}
+
+		resultado := Soma(numeros)
+		esperado := 6
+
+		if resultado != esperado {
+			t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
+		}
+	})
+
 }
